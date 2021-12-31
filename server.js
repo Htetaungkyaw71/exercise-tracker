bdcdd543f4144fedd9555ee3d40fff271d04525b
hteta <htetaung200071@gmail.com>
2021-12-31 23:30:27 +0630

    initial commit

--- server.js
+++ server.js
@@ -49,11 +49,11 @@ app.post('/api/users',(req,res)=>{
 })
 
 
-var logs = []
+
 var l = []
+
 app.post('/api/users/:_id/exercises',(req,res)=>{
   var today = new Date()
-  // var options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
     var d = req.body.date;
     if(d == ""){
       d = today.toDateString()
@@ -61,64 +61,97 @@ app.post('/api/users/:_id/exercises',(req,res)=>{
       var d = new Date(req.body.date).toDateString();
     }
 
-    users.forEach(function(user){
-      console.log(user._id)
-      console.log(req.body._id)
-      if (user._id == req.body._id ){
-        l.push(
-          {
-            description:  req.body.description,
-            duration: parseInt(req.body.duration),
-            date: d,           
-          }
-        )
-        user.logs = l
+  const user = users.filter(user => user._id == req.body._id);
+    l.push(
+      {
+        description:  req.body.description,
+        duration: parseInt(req.body.duration),
+        date: d,           
+      }
+    )
+    user[0].logs = l
+
+
+    res.json({
+      username: user.username,
+      description:  req.body.description,
+      duration: parseInt(req.body.duration),
+      date: d,
+      _id: req.body._id ,          
+    })
+
+    // users.forEach(function(user){
+    //   if (user._id == req.body._id ){
+    //     l.push(
+    //       {
+    //         description:  req.body.description,
+    //         duration: parseInt(req.body.duration),
+    //         date: d,           
+    //       }
+    //     )
+    //     user.logs = l
       
     
-        res.json({
-          username: user.username,
-          description:  req.body.description,
-          duration: parseInt(req.body.duration),
-          date: d,
-          _id: req.body._id ,          
-        })
-      }else{
-        console.log("error not found")
-      }
-    })
+     
+    //   }
+    // })
 
 
 
 })
 
+
 app.get("/api/users/:_id/logs",(req,res)=>{
   const {from,to,limit} = req.query
   var  user_id = req.params._id
-  console.log(user_id)
-  users.forEach(function(user){
-    if(user._id == user_id){
-      if(from){
-        fromdate = new Date(from)
-        user.logs = user.logs.filter(exe => new Date(exe.date) > fromdate)
-      }
-      if(to){
-        todate = new Date(to)
-        user.logs = user.logs.filter(exe => new Date(exe.date) < todate)
-      }
-      if(limit){
-        user.logs = user.logs.slice(0,+limit)
+  const user = users.filter(user => user._id == user_id);
+ 
+  if(from){
+    fromdate = new Date(from)
+    user[0].logs = user[0].logs.filter(exe => new Date(exe.date) > fromdate)
+  }
+  if(to){
+    todate = new Date(to)
+    user[0].logs = user[0].logs.filter(exe => new Date(exe.date) < todate)
+  }
+  if(limit){
+    user[0].logs = user[0].logs.slice(0,+limit)
+    
+  }
+ res.json(
+  {
+    username:user[0].username,
+    count: user[0].logs.length,
+    _id:user[0]._id,
+    log:user[0].logs
+  }
+ )
+
+
+  // users.forEach(function(user){
+  //   if(user._id == user_id){
+  //     if(from){
+  //       fromdate = new Date(from)
+  //       user.logs = user.logs.filter(exe => new Date(exe.date) > fromdate)
+  //     }
+  //     if(to){
+  //       todate = new Date(to)
+  //       user.logs = user.logs.filter(exe => new Date(exe.date) < todate)
+  //     }
+  //     if(limit){
+  //       user.logs = user.logs.slice(0,+limit)
         
-      }
-     res.json(
-      {
-        username:user.username,
-        count: user.logs.length,
-        _id:user._id,
-        log:user.logs
-      }
-     )
-    }
-  })
+  //     }
+  //    res.json(
+  //     {
+  //       username:user.username,
+  //       count: user.logs.length,
+  //       _id:user._id,
+  //       log:user.logs
+  //     }
+  //    )
+  //   }
+  // })
 })
 
 
